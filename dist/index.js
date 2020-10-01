3319757cec679b398dced0a14dc28dc38ace574b
prayans <tyagipriyanshu077@gmail.com>
2020-10-01 12:33:00 +0530

    Library

--- dist/index.js
+++ dist/index.js
@@ -12,6 +12,7 @@ var TimePicker = require('@material-ui/pickers/TimePicker');
 var lodash = require('lodash');
 var core = require('@material-ui/core');
 var moment = _interopDefault(require('moment'));
+var Utils = require('react-forms/dist/lib/ml-form-builder/Utils');
 
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
@@ -92,14 +93,6 @@ var MUITimePicker = function (props) {
     return (React.createElement(TimePicker.KeyboardTimePicker, __assign({}, updatedProps)));
 };
 
-var getFieldError = function (fieldName, formikProps) {
-    var fieldError = lodash.get(formikProps, "errors." + fieldName);
-    var isTouched = lodash.get(formikProps, "touched." + fieldName);
-    if (!isTouched && formikProps.submitCount < 1)
-        return '';
-    return fieldError;
-};
-
 var getOptions = function (startTime, endTime, interval, amPm) {
     var start = amPm ? moment(startTime, 'hh:mm a').toDate() : moment(startTime, 'HH:mm').toDate();
     var end = amPm ? moment(endTime, 'hh:mm a').toDate() : moment(endTime, 'HH:mm').toDate();
@@ -113,7 +106,7 @@ var getOptions = function (startTime, endTime, interval, amPm) {
 };
 var MUIDropDownTimePicker = function (props) {
     var _a = props.fieldProps, fieldProps = _a === void 0 ? {} : _a, _b = props.fieldConfig, fieldConfig = _b === void 0 ? {} : _b, _c = props.formikProps, formikProps = _c === void 0 ? {} : _c;
-    var fieldError = getFieldError((fieldProps.name || ''), formikProps);
+    var fieldError = Utils.getFieldError((fieldProps.name || ''), formikProps);
     var _d = fieldProps.formControlProps, formControlProps = _d === void 0 ? {} : _d, _e = fieldProps.startTime, startTime = _e === void 0 ? '00:00' : _e, _f = fieldProps.endTime, endTime = _f === void 0 ? '23:45' : _f, _g = fieldProps.interval, interval = _g === void 0 ? 15 : _g, _h = fieldProps.amPm, amPm = _h === void 0 ? false : _h, label = fieldProps.label, emptyItem = fieldProps.emptyItem, helperText = fieldProps.helperText, _j = fieldProps.inputLabelProps, inputLabelProps = _j === void 0 ? {} : _j, formHelperTextProps = fieldProps.formHelperTextProps, _k = fieldProps.menuItemProps, menuItemProps = _k === void 0 ? {} : _k, _l = fieldProps.emptyMenuItemProps, emptyMenuItemProps = _l === void 0 ? {} : _l, _m = fieldProps.error, error = _m === void 0 ? !!fieldError : _m, selectProps = __rest(fieldProps, ["formControlProps", "startTime", "endTime", "interval", "amPm", "label", "emptyItem", "helperText", "inputLabelProps", "formHelperTextProps", "menuItemProps", "emptyMenuItemProps", "error"]);
     var labelId = fieldConfig.id + "_label";
     var value = lodash.get(formikProps, "values." + fieldProps.name) || '';
