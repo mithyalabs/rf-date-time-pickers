4384db2a7a82838830ac2f2c912ba365837fb67d
prayans <tyagipriyanshu077@gmail.com>
2020-10-05 12:25:57 +0530

    change  Date to ISO formate

--- src/lib/MUIDateTimePicker.tsx
+++ src/lib/MUIDateTimePicker.tsx
@@ -21,7 +21,7 @@ export const MUIDatePicker: React.FC<IFieldProps & { fieldProps?: IMUIDatePicker
             formikProps.setFieldValue(fieldProps.name, date, false);
         }
         else{
-            let data = new Date(date)
+            let data = new Date(date).toISOString();
             // (outputFormat === 'date') ? date :moment(date).format(outputFormat || fieldProps.format || 'MM/DD/YYYY')
             formikProps.setFieldValue(fieldProps.name, data , false);
         }    
